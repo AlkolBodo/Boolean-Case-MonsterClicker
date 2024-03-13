b60c47e7ad71b602e33bee162dab49ceef2af01a
Benjamin A. E. Darici <benjamin.darici@no.experis.com>
2024-03-13 13:46:12 +0100

    Fixed blurring issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <img className="icon" src={MBread} alt="Mystery Bread"  width="256" height="256" onClick={() => setCount((count) => count + 1)}/>
+        <img className="icon" src="https://raw.githubusercontent.com/AlkolBodo/Boolean-Case-MonsterClicker/main/src/assets/Mysterious%20Bread.png" alt="Mystery Bread"  width="256" height="256" onClick={() => setCount((count) => count + 1)}/>
       </div>
       <h1>CLICK HIM</h1>
       <div className="card">
